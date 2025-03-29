42ce86b83d71b5d752199d8fd32a9da36615017d
hubermanophir <ofirhuberman@gmail.com>
2025-03-29 14:43:53 +0300

    added basic pages fetch for products and product page

--- src/pages/products/[id].tsx
+++ src/pages/products/[id].tsx
@@ -1,8 +1,30 @@
 import { ProductQuery } from "@/types/productsTypes";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 export default function Page() {
   const router = useRouter();
   const query = router.query as ProductQuery;
-  return <p>Post: {query.id}</p>;
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (query.id) {
+      fetch(`/api/products/${query.id}`)
+        .then((response) => response.json())
+        .then((data) => {
+          setData(data);
+          setLoading(false);
+        });
+    }
+  }, [query.id]);
+
+  if (loading) return <p>Loading...</p>;
+
+  return (
+    <div>
+      <h1>Data from API</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
 }
--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,5 +1,28 @@
-
+import { ProductQuery } from "@/types/productsTypes";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 export default function Page() {
-  return <h1>Posts</h1>;
+  const router = useRouter();
+  const query = router.query as ProductQuery;
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(`/api/products/`)
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+
+  return (
+    <div>
+      <h1>Data from API</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
 }
